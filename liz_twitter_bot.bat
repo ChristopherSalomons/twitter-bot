e8b47f072d24c27717176954c681ecc569d7f81e
Christopher Salomons <christopher.salomons@gmail.com>
2023-08-15 23:59:39 -0400

    initial commit

--- liz_twitter_bot.bat
+++
@@ -1,2 +0,0 @@
-cd D:\Chris\Documents\VSCodeProjects\twitter-bot
-"D:\Chris\Documents\VSCodeProjects\twitter-bot\.venv\Scripts\python.exe" "D:\Chris\Documents\VSCodeProjects\twitter-bot\liz_twitter_bot.py"
